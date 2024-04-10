b1209a515b430b9266d0ebcc61102afe952b0f3e
5kydriven <tagpuno619@gmail.com>
2024-04-10 10:43:08 +0800

    few modifications

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import { RouterView } from "vue-router";
+import { useAuthStore } from "./stores/AuthStore";
+import { onMounted } from "vue";
+
+const authStore = useAuthStore();
+
+onMounted(() => {
+  authStore.init()
+})
 </script>
 
 <template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import SigninView from '../views/SigninView.vue'
+import MainPage from '../views/MainPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
-    }
+      name: 'signin',
+      component: SigninView
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: MainPage
+    },
   ]
 })
 
