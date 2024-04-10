58be42990b88b43caecea589c11b309941c8bc76
5kydriven <tagpuno619@gmail.com>
2024-04-10 10:42:00 +0800

    added flowbite and few tweaks

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { initFlowbite } from "flowbite";
 
 import App from "./App.vue";
 import router from "./router";
 
-import '@/assets/tailwind.css'
+import "@/assets/tailwind.css";
+
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(initFlowbite());
 
 app.mount("#app");
