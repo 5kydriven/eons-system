c57258b6e53230a7e678de17c226e6619c36b97b
5kydriven <tagpuno619@gmail.com>
2024-04-11 09:00:51 +0800

    remove init flowbite

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import { initFlowbite } from "flowbite";
 
 import App from "./App.vue";
 import router from "./router";
@@ -12,6 +11,5 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(initFlowbite());
 
 app.mount("#app");
