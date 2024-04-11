9dabd946ec24d7ad739ff974705b964b9449ee1e
5kydriven <tagpuno619@gmail.com>
2024-04-11 09:02:22 +0800

    using async/await instead of promise

--- src/stores/AuthStore.js
+++ src/stores/AuthStore.js
@@ -6,9 +6,11 @@ import {
 import { defineStore } from "pinia";
 import { auth } from "./firebase";
 import { ref } from "vue";
+import { useRouter } from "vue-router";
 
 export const useAuthStore = defineStore("authStore", () => {
   const user = ref({});
+  const router = useRouter();
 
   const init = () => {
     onAuthStateChanged(auth, (userDetails) => {
@@ -19,32 +21,56 @@ export const useAuthStore = defineStore("authStore", () => {
           email: userDetails.email,
           uid,
         };
+        router.push({ name: "dashboard" });
       } else {
         user.value = {};
+        router.replace({ name: "signin" });
       }
     });
   };
 
-  const registerUser = (credentials) => {};
+  const loginUser = async (credentials) => {
+    try {
+      const userCredentials = await signInWithEmailAndPassword(auth, credentials.email, credentials.password);
+      // for debugging
+      console.log(userCredentials.user);
+    } catch (err) {
+      const errCode = err.code;
+      let errMessage;
 
-  const loginUser = (credentials) => {
-    signInWithEmailAndPassword(auth, credentials.email, credentials.password)
-      .then((userCredentials) => {
-        const user = userCredentials.user;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      switch (errCode) {
+        case "auth/invalid-email":
+          errMessage = "Email is invalid";
+          break;
+        case "auth/user-not-found":
+          errMessage = "User is not registered.";
+          break;
+        case "auth/wrong-password":
+          errMessage = "Wrong password.";
+          break;
+        case "auth/invalid-credential":
+          errMessage = "Incorrect email or password";
+          break;
+        default:
+          console.log(err.code);
+          errMessage = "Unknown error";
+      }
+
+      return { error: true, message: errMessage }
+    }
   };
 
-  const logoutUser = () => {
-    signOut(auth)
-      .then(() => {})
-      .catch((err) => {});
+
+  const logoutUser = async () => {
+    try {
+      await signOut(auth);
+      console.log("User logged out successfully");
+    } catch (err) {
+      console.error("Error logging out:", err);
+    }
   };
 
   return {
-    registerUser,
     loginUser,
     logoutUser,
     init,
