ea2e8ddaa6479b5eb61aa2886d330aa413a1a9b0
5kydriven <tagpuno619@gmail.com>
2024-04-11 09:08:32 +0800

    added route guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SigninView from '../views/SigninView.vue'
 import MainPage from '../views/MainPage.vue'
+import { useAuthStore } from '@/stores/AuthStore'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,14 +9,34 @@ const router = createRouter({
     {
       path: '/',
       name: 'signin',
-      component: SigninView
+      component: SigninView,
+      meta: {
+        auth: false
+      }
     },
     {
       path: '/dashboard',
       name: 'dashboard',
-      component: MainPage
+      component: MainPage,
+      meta: {
+        auth: true
+      }
     },
   ]
 })
+
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore(); // Access the authStore instance
+  const { isAuthenticated, user } = authStore;
+
+  if (to.meta.auth && !isAuthenticated) {
+    next({ name: 'signin' });
+  } else if (user.uid && to.name === 'signin') {
+    next({ name: 'dashboard' });
+  } else {
+    next();
+  }
+});
 
 export default router
