5f62205bc71d2485dbf402d3766f47546f2acd23
5kydriven <tagpuno619@gmail.com>
2024-04-14 16:51:38 +0800

    added some few designs

--- src/components/pages/DashboardPage.vue
+++ src/components/pages/DashboardPage.vue
@@ -1,35 +1,51 @@
 <template>
   <div class="p-4 sm:ml-64">
-    <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-      <div class="grid grid-cols-3 gap-4 mb-4">
-        <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-          <p class="text-2xl text-gray-400 dark:text-gray-500">
-            <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-              viewBox="0 0 18 18">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M9 1v16M1 9h16" />
-            </svg>
-          </p>
-        </div>
-        <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-          <p class="text-2xl text-gray-400 dark:text-gray-500">
-            <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-              viewBox="0 0 18 18">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M9 1v16M1 9h16" />
-            </svg>
-          </p>
-        </div>
-        <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-          <p class="text-2xl text-gray-400 dark:text-gray-500">
-            <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-              viewBox="0 0 18 18">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M9 1v16M1 9h16" />
-            </svg>
-          </p>
+    <div class="p-3 dark:border-gray-700 mt-14">
+      <h1 class="text-2xl md:text-3xl font-bold">Hello, Eon! </h1>
+      <label class="text-gray-500 text-xs md:text-sm ">Here's an overview of what is happening to your store.</label>
+
+      <div class="md:border rounded-sm shadow-sm border-gray-200 mb-4 md:p-4 mt-2">
+        <div class="grid grid-cols-2 md:grid-cols-4 md:gap-2 gap-2 ">
+
+          <div class="flex flex-col h-24 md:h-28 bg-gray-50 md:bg-white p-3 md:border-r justify-between border-gray-200 dark:bg-gray-800">
+            <label class="text-xs font-medium  md:text-base">Gross Sales</label>
+            <div>
+              <h1 class="text-md font-semibold md:font-bold md:text-2xl">₱ 10,000</h1>
+              <label class="text-xs font-normal md:font-semibold">26%<span class="text-xs font-thin text-gray-300 ml-2">+ 1.2k today</span></label>
+            </div>
+          </div> 
+
+          <div class="flex flex-col h-24 md:h-28 bg-gray-50 md:bg-white p-3 md:border-r justify-between border-gray-200 dark:bg-gray-800">
+            <label class="text-xs font-medium  md:text-base">Gross Sales</label>
+            <div>
+              <h1 class="text-md font-semibold md:font-bold md:text-2xl">₱ 10,000</h1>
+              <label class="text-xs font-normal md:font-semibold">26%<span class="text-xs font-thin text-gray-300 ml-2">+ 1.2k today</span></label>
+            </div>
+          </div> 
+
+          <div class="flex flex-col h-24 md:h-28 bg-gray-50 md:bg-white p-3 md:border-r justify-between border-gray-200 dark:bg-gray-800">
+            <label class="text-xs font-medium  md:text-base">Gross Sales</label>
+            <div>
+              <h1 class="text-md font-semibold md:font-bold md:text-2xl">₱ 10,000</h1>
+              <label class="text-xs font-normal md:font-semibold">26%<span class="text-xs font-thin text-gray-300 ml-2">+ 1.2k today</span></label>
+            </div>
+          </div> 
+
+          <div class="flex flex-col h-24 md:h-28 bg-gray-50 md:bg-white p-3 justify-between dark:bg-gray-800">
+            <label class="text-xs font-medium  md:text-base">Gross Sales</label>
+            <div>
+              <h1 class="text-md font-semibold md:font-bold md:text-2xl">₱ 10,000</h1>
+              <label class="text-xs font-normal md:font-semibold">26%<span class="text-xs font-thin text-gray-300 ml-2">+ 1.2k today</span></label>
+            </div>
+          </div> 
+
+           
+
+            
+
         </div>
       </div>
+
       <div class="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
         <p class="text-2xl text-gray-400 dark:text-gray-500">
           <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -39,6 +55,7 @@
           </svg>
         </p>
       </div>
+
       <div class="grid grid-cols-2 gap-4 mb-4">
         <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
           <p class="text-2xl text-gray-400 dark:text-gray-500">
@@ -77,53 +94,7 @@
           </p>
         </div>
       </div>
-      <div class="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-        <p class="text-2xl text-gray-400 dark:text-gray-500">
-          <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-            viewBox="0 0 18 18">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M9 1v16M1 9h16" />
-          </svg>
-        </p>
-      </div>
-      <div class="grid grid-cols-2 gap-4">
-        <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-          <p class="text-2xl text-gray-400 dark:text-gray-500">
-            <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-              viewBox="0 0 18 18">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M9 1v16M1 9h16" />
-            </svg>
-          </p>
-        </div>
-        <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-          <p class="text-2xl text-gray-400 dark:text-gray-500">
-            <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-              viewBox="0 0 18 18">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M9 1v16M1 9h16" />
-            </svg>
-          </p>
-        </div>
-        <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-          <p class="text-2xl text-gray-400 dark:text-gray-500">
-            <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-              viewBox="0 0 18 18">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M9 1v16M1 9h16" />
-            </svg>
-          </p>
-        </div>
-        <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-          <p class="text-2xl text-gray-400 dark:text-gray-500">
-            <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-              viewBox="0 0 18 18">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M9 1v16M1 9h16" />
-            </svg>
-          </p>
-        </div>
-      </div>
+
     </div>
   </div>
 </template>
