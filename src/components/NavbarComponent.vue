7373a907a0197717ec882c4a7845accc3cc562fb
5kydriven <tagpuno619@gmail.com>
2024-04-25 20:34:23 +0800

    added some logo

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -23,8 +23,8 @@ const onLogout = () => {
               </path>
             </svg>
           </button>
-          <a href="https://flowbite.com" class="flex ms-2 md:me-24">
-            <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 me-3" alt="FlowBite Logo" />
+          <a class="flex ms-2 md:me-24">
+            <img src="/logo.svg" class="h-8 me-3" />
             <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">EON'S</span>
           </a>
         </div>
