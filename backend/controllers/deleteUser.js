daa77a95719bcd9ca7e1c2dc964d94329f6c0118
5kydriven <tagpuno619@gmail.com>
2024-06-24 23:32:28 +0800

    few modifications

--- backend/controllers/deleteUser.js
+++ backend/controllers/deleteUser.js
@@ -4,7 +4,7 @@ import admin from 'firebase-admin';
 export const deleteUser = async (req, res) => {
     const { uid } = req.body;
     try {
-        const user = await admin.auth().deleteUser(uid);
+        await admin.auth().deleteUser(uid);
         res.status(200).send({ message: "Successfully deleted" });
     } catch (error) {
         res.status(400).send(error.message);
