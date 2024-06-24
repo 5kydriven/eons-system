5323d7bd1719bc1eb0511bc418f6974b80593af5
5kydriven <tagpuno619@gmail.com>
2024-06-24 23:31:09 +0800

    YAGNI

--- frontend/src/components/adminLayout/AdminNavbar.vue
+++ frontend/src/components/adminLayout/AdminNavbar.vue
@@ -49,16 +49,17 @@ const OpToggle = (event) => {
                     </button>
                     <div class="flex items-center ms-3">
                         <div>
-                            <button type="button" @click="OpToggle"
+                            <!-- <button type="button" @click="OpToggle"
                                 class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
                                 aria-expanded="false" data-dropdown-toggle="dropdown-user">
                                 <span class="sr-only">Open user menu</span>
                                 <img class="w-8 h-8 rounded-full"
                                     src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
                                     alt="user photo" />
-                            </button>
+                            </button> -->
+                            <Button label="Logout" severity="danger" size="small" outlined icon="pi pi-sign-out" />
                         </div>
-                        <OverlayPanel ref="op">
+                        <!-- <OverlayPanel ref="op">
                             <div class="text-base list-none bg-white">
                                 <div class="px-4 py-3" role="none">
                                     <p class="text-sm text-gray-900 dark:text-white" role="none">
@@ -83,7 +84,7 @@ const OpToggle = (event) => {
                                     </li>
                                 </ul>
                             </div>
-                        </OverlayPanel>
+                        </OverlayPanel> -->
                     </div>
                 </div>
             </div>
