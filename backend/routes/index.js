f4123210ff055c8452f447ae92828c8a0fb09a89
5kydriven <tagpuno619@gmail.com>
2024-07-02 19:28:45 +0800

    modify cors

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,4 +1,3 @@
-// routes/index.js
 import express from 'express';
 import { createUser } from '../controllers/createUser.js';
 import { deleteUser } from '../controllers/deleteUser.js';
@@ -10,6 +9,14 @@ router.get("/", (req, res) => {
 });
 
 router.post("/createUser", createUser);
+
+// Add this route to handle preflight requests
+router.options('/deleteUser', (req, res) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Methods', 'POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.sendStatus(200);
+});
 
 router.post("/deleteUser", deleteUser);
 
