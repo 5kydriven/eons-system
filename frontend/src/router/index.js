10c314668f6fc0a8538dcedea044dc2efaf63e19
5kydriven <tagpuno619@gmail.com>
2024-07-02 18:10:48 +0800

    minor changes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import AuthPage from '@/pages/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +6,10 @@ const router = createRouter({
     {
       path: '/',
       name: 'auth',
-      component: AuthPage
+      component: () => import('@/pages/index.vue'),
+      meta: {
+        title: "EON's"
+      }
     },
     {
       path: '/dashboard',
