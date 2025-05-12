c77606ce7fb4778d501af6de8694699a378e7297
5kydriven <tagpuno619@gmail.com>
2025-05-12 12:16:16 +0800

    feat: update dependencies and integrate PrimeVue with TailwindCSS
    - Added PrimeVue, PrimeIcons, TailwindCSS, and PrimeUI theme to dependencies.
    - Removed unused components and styles, including HelloWorld, TheWelcome, and AboutView.
    - Simplified App.vue by removing header and navigation.
    - Replaced HomeView.vue with a new index.vue page that includes a button.
    - Updated router to point to the new index.vue page.
    - Removed base.css and integrated TailwindCSS styles in main.css.
    - Configured PrimeVue in main.ts with a preset theme.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,27 @@
 import './assets/main.css'
+import 'primeicons/primeicons.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
 
 import App from './App.vue'
 import router from './router'
+import Button from 'primevue/button'
 
 const app = createApp(App)
+
+app.component('Button', Button)
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: false || 'none',
+    },
+  },
+})
 
 app.use(createPinia())
 app.use(router)
---
+++ src/pages/index.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1 class="text-red-500">Hello World</h1>
+  <Button label="Click Me" icon="pi pi-check" />
+</template>
